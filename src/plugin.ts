5822a3dacbfa1eb82a2dbc43e5dbc3807b2fcba7
Wes Lowe <wesley.lowe@yahoo.com>
2025-06-01 23:21:39 -0500

    began using limbo openai utils, fixed the build accidentially being included in version control

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,37 @@
 import * as limbo from "limbo";
-import { OpenAIClient } from "openai-fetch";
+import {
+	FetchAdapter,
+	OpenAICompatibleClient,
+	streamOpenAICompatibleChatCompletion,
+} from "@limbo/openai-utils";
 
-/**
- * Considerations:
- * - I will probably end up making an openai-compatible utility for plugins to use, including this one
- */
+const OPENAI_API_BASE_URL = "https://api.openai.com/v1";
 
-const models = [
+interface LLMModelConfig {
+	id: string;
+	name: string;
+	description: string;
+	capabilities: limbo.LLM.Capability[];
+}
+
+const models: LLMModelConfig[] = [
 	{
 		id: "o4-mini",
 		name: "o4-mini",
 		description: "Faster, more affordable reasoning model",
+		capabilities: ["tool_calling", "structured_outputs"],
+	},
+	{
+		id: "o3",
+		name: "o3",
+		description: "Our most powerful reasoning model",
+		capabilities: ["tool_calling", "structured_outputs"],
+	},
+	{
+		id: "o3-mini",
+		name: "o3-mini",
+		description: "A small model alternative to o3",
+		capabilities: [],
 	},
 ] as const;
 
@@ -18,7 +39,7 @@ export default {
 	onActivate: async () => {
 		console.log("OpenAI plugin activated");
 
-		// --- settings ---
+		// settings
 
 		limbo.settings.register({
 			id: "api_key",
@@ -29,49 +50,34 @@ export default {
 			variant: "password",
 		});
 
+		// models
+
 		for (const model of models) {
 			limbo.models.registerLLM({
 				id: model.id,
 				name: model.name,
 				description: model.description,
-				generateText: async ({ promptBuilder, onChunk }) => {
+				capabilities: model.capabilities,
+				streamText: async ({ tools, messages, onText, onToolCall }) => {
 					const apiKey = limbo.settings.get("api_key");
 
 					if (typeof apiKey !== "string") {
-						return limbo.notifications.show({
-							type: "warning",
-							message: "You must provide an API key to use OpenAI models",
-						});
+						throw new Error("You must provide an API key to use OpenAI models");
 					}
 
-					const client = new OpenAIClient({ apiKey });
-
-					const response = await client.streamChatCompletion({
-						model: model.id,
-						messages: promptBuilder.getMessages(),
+					const client = new OpenAICompatibleClient({
+						adapter: new FetchAdapter(),
+						baseUrl: OPENAI_API_BASE_URL,
+						apiKey,
 					});
 
-					const reader = response.getReader();
-
-					while (true) {
-						const readResult = await reader.read();
-
-						if (readResult.done) {
-							break;
-						}
-
-						const chunk = readResult.value;
-
-						if (!chunk) {
-							return;
-						}
-
-						const chunkText = chunk.choices[0]?.delta.content;
-
-						if (chunkText) {
-							onChunk(chunkText);
-						}
-					}
+					await streamOpenAICompatibleChatCompletion(client, {
+						model: model.id,
+						tools,
+						messages,
+						onText,
+						onToolCall,
+					});
 				},
 			});
 		}
